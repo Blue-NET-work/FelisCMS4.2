049e1be4d7839bc4a5d642f818f8b5a6bcc750c0
Mateusz Serwinowski <biuro@blue-net.pl>
2014-03-12 18:10:01 +0100

    pages component start

--- _admin/controllers/Pages.php
+++ _admin/controllers/Pages.php
@@ -1,15 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Pages extends FC_Controller {
 
     function __construct(){
         parent::__construct();  
-        @FC_Request::loadModel('admin_model');                                                                        
+        @FC_Request::loadModel('admin_model'); 
+        @FC_Request::loadLang('felis_pages');                                                                       
     }
     
-    public function index(){
+    public function index(){ 
         
+        @FC_Request::smartyView('pages/list.tpl');
     }
     
 }
