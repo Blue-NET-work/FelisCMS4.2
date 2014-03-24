825b9e8c79bbc4610cd12e3db4694159cb9d6a41
Mateusz Serwinowski <biuro@blue-net.pl>
2014-03-24 15:00:48 +0100

    FC_Controller

--- cms/core/FC_Controller.php
+++ cms/core/FC_Controller.php
@@ -1,23 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class FC_Controller extends CI_Controller {
- /*   
+
     public function __construct(){
-        parent::__construct();  
-        $this->__loginStatus();   
+        parent::__construct();
+        $this->__loginStatus();
     }
-    
+
     private function __loginStatus(){
-        $siteStatus = @FC_DB::getDataSelect('value', 'config', '`name` = "service_offline"');         
+        $siteStatus = $this->db->get_where('config', array("name"=>"offline"))->row("value");
         if($siteStatus == 'TRUE'){
             if($this->ion_auth->is_admin() AND $this->ion_auth->is_admin()) $this->__offlineLogIn();
             else $this->__offline();
         }
     }
-    
+
     private function __offline(){
-        $startDate = @FC_DB::getDataSelect('value', 'config', '`name` = "service_offline_startdate"');       
-        $endDate = @FC_DB::getDataSelect('value', 'config', '`name` = "service_offline_enddate"'); 
+        $startDate = @FC_DB::getDataSelect('value', 'config', '`name` = "offline_startdate"');
+        $endDate = @FC_DB::getDataSelect('value', 'config', '`name` = "offline_enddate"');
         $lang["site_offline_back"]    = lang("site_offline_back");
         $lang["site_offline_days"]    = lang("site_offline_days");
         $lang["site_offline_hours"]   = lang("site_offline_hours");
@@ -25,16 +25,15 @@ class FC_Controller extends CI_Controller {
         $lang["site_offline_seconds"] = lang("site_offline_seconds");
         $lang["site_offline_build"]   = lang("site_offline_build");
         $lang["site_offline_build"]   = lang("site_offline_build");
-        
-        require_once(BASEPATH.'templates/clock.php');
+        require_once(VIEWPATH.'/clock.php');
         exit;
     }
-    
+
     private function __offlineLogIn(){
-        $system_url = system_url("/"); 
-        $lang = lang("site_offline");   
-        $messages = "<div style='position:fixed; width:58px; height:58px; top:0px; left:0px; z-index:99999;'><img style='width:58px; height:58px;' src='{$system_url}views/assets/img/clock/offline.png' alt='offline' title='{$lang}'></div>";
+        $system_url = system_url("/");
+        $lang = lang("site_offline");
+        $messages = "<div style='position:fixed; width:58px; height:58px; top:0px; left:0px; z-index:99999;'><img style='width:58px; height:58px;' src='{$system_url}templates/assets/img/clock/offline.png' alt='offline' title='{$lang}'></div>";
         print($messages);
     }
-    */       
+
 }
