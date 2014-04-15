f30ffcbb99bf899dbbedc5346d2632789409d3af
Mateusz Serwinowski <biuro@blue-net.pl>
2014-04-15 22:33:34 +0200

    update

--- _admin/index.php
+++ _admin/index.php
@@ -46,104 +46,104 @@
  * ---------------------------------------------------------------
  */
 
-    // Set the current directory correctly for CLI requests
-    if (defined('STDIN'))
-    {
-        chdir(dirname(__FILE__));
-    }
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
+	}
 
-    if (($_temp = realpath($system_path)) !== FALSE)
-    {
-        $system_path = $_temp.'/';
-    }
-    else
-    {
-        // Ensure there's a trailing slash
-        $system_path = rtrim($system_path, '/').'/';
-    }
+	if (($_temp = realpath($system_path)) !== FALSE)
+	{
+		$system_path = $_temp.'/';
+	}
+	else
+	{
+		// Ensure there's a trailing slash
+		$system_path = rtrim($system_path, '/').'/';
+	}
 
-    // Is the system path correct?
-    if ( ! is_dir($system_path))
-    {
-        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-        echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-        exit(3); // EXIT_* constants not yet defined; 3 is EXIT_CONFIG.
-    }
+	// Is the system path correct?
+	if ( ! is_dir($system_path))
+	{
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+		exit(3); // EXIT_CONFIG
+	}
 
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-    // The name of THIS file
-    define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
     // Path to the root folder
     define('ROOTPATH', str_replace('\\', '/', $root_path));
 
-    // Path to the system folder
-    define('BASEPATH', str_replace('\\', '/', $system_path));
-
     // Path to the uploads folder
     define('UPLOADSPATH', str_replace('\\', '/', $uploads_folder));
 
-    // Path to the front controller (this file)
-    define('FCPATH', str_replace(SELF, '', __FILE__));
+	// Path to the system folder
+	define('BASEPATH', str_replace('\\', '/', $system_path));
 
-    // Name of the "system folder"
-    define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+	// Path to the front controller (this file)
+	define('FCPATH', str_replace(SELF, '', __FILE__));
 
-    // The path to the "application" folder
-    if (is_dir($application_folder))
-    {
-        if (($_temp = realpath($application_folder)) !== FALSE)
-        {
-            $application_folder = $_temp;
-        }
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
-        define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
-    }
-    else
-    {
-        if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-        {
-            header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-            echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-            exit(3); // EXIT_* constants not yet defined; 3 is EXIT_CONFIG.
-        }
+	// The path to the "application" folder
+	if (is_dir($application_folder))
+	{
+		if (($_temp = realpath($application_folder)) !== FALSE)
+		{
+			$application_folder = $_temp;
+		}
 
-        define('APPPATH', BASEPATH.$application_folder.DIRECTORY_SEPARATOR);
-    }
+		define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+	}
+	else
+	{
+		if ( ! is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
+		{
+			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+			echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+			exit(3); // EXIT_CONFIG
+		}
 
-    // The path to the "views" folder
-    if ( ! is_dir($view_folder))
-    {
-        if ( ! empty($view_folder) && is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-        {
-            $view_folder = APPPATH.$view_folder;
-        }
-        elseif ( ! is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-        {
-            header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-            echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-            exit(3); // EXIT_* constants not yet defined; 3 is EXIT_CONFIG.
-        }
-        else
-        {
-            $view_folder = APPPATH.'views';
-        }
-    }
+		define('APPPATH', BASEPATH.$application_folder.DIRECTORY_SEPARATOR);
+	}
 
-    if (($_temp = realpath($view_folder)) !== FALSE)
-    {
-        $view_folder = $_temp.DIRECTORY_SEPARATOR;
-    }
-    else
-    {
-        $view_folder = rtrim($view_folder, '/\\').DIRECTORY_SEPARATOR;
-    }
+	// The path to the "views" folder
+	if ( ! is_dir($view_folder))
+	{
+		if ( ! empty($view_folder) && is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
+		{
+			$view_folder = APPPATH.$view_folder;
+		}
+		elseif ( ! is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+		{
+			header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+			echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+			exit(3); // EXIT_CONFIG
+		}
+		else
+		{
+			$view_folder = APPPATH.'views';
+		}
+	}
 
-    define('VIEWPATH', $view_folder);
+	if (($_temp = realpath($view_folder)) !== FALSE)
+	{
+		$view_folder = $_temp.DIRECTORY_SEPARATOR;
+	}
+	else
+	{
+		$view_folder = rtrim($view_folder, '/\\').DIRECTORY_SEPARATOR;
+	}
+
+	define('VIEWPATH', $view_folder);
 
 /*
  * --------------------------------------------------------------------
